2f8d448203e46ae339b98f7e55a6dc80d9eba6e5
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-08-18 12:00:14 -0300

    fixed spacing

--- src/components/app-hero.tsx
+++ src/components/app-hero.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import { motion } from "framer-motion";
 import { AppSocialLinks } from "./app-sociallinks";
 import { supabase } from "@/lib/supabase";
