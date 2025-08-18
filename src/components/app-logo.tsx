21d10458b16b8c89d4eabbaf2aad582ac8072bf9
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-08-18 12:14:39 +0800

    added .env

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,13 +1,21 @@
-import { baseUrl } from "@/config/app"
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+
+import { supabase } from "@/lib/supabase";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export function AppLogo() {
-    return (
-        <div className='flex items-center gap-2'>
-            <Avatar className="hidden md:h-9 md:w-12 md:flex p-1">
-                <AvatarImage src={baseUrl + '/avatars/logo.png'} alt='logo' />
-                <AvatarFallback className="rounded-lg">SC</AvatarFallback>
-            </Avatar>
-        </div>
-    )
+  const { data } = supabase
+    .storage
+    .from("portfolio-assets")
+    .getPublicUrl("logo.png");
+  
+  console.log("Logo URL:", data.publicUrl);
+
+  return (
+    <div className="flex items-center gap-2">
+      <Avatar className="hidden md:h-9 md:w-12 md:flex p-1">
+        <AvatarImage src={data.publicUrl} alt="logo" />
+        <AvatarFallback className="rounded-lg">SC</AvatarFallback>
+      </Avatar>
+    </div>
+  );
 }
