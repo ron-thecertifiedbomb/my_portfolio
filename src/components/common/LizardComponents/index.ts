7d9cb04e0f6c29db241fd5bb7c3e7c4ae760812e
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-08-24 17:32:09 +0800

    safe

--- src/components/common/LizardComponents/index.ts
+++ src/components/common/LizardComponents/index.ts
@@ -1,5 +1,7 @@
+
 export { LizardCard } from "./LizardCard";
 export { LizardText } from "./LizardText";
 export { LizardCenterCard } from "./LizardCenterCard";
 export { LizardImage } from "./LizardImage";
 export { LizardCardGrid } from "./LizardCardGrid";
+export { CraftmanshipSection } from "./sections/Craftmanship";
---
+++ src/components/common/LizardComponents/sections/Craftmanship.tsx
@@ -0,0 +1,48 @@
+import { AppMainContainer, AppSectionContainer } from "@/components/layout";
+import { LizardCard } from "@/components/common";
+import {
+  LizardCardGrid,
+  LizardCenterCard,
+  LizardText,
+} from "@/components/common/LizardComponents";
+import { centerCard } from "@/utils/lizardinteractivemockdata";
+
+export function CraftmanshipSection() {
+  return (
+    <AppMainContainer className="w-screen h-screen flex items-center justify-center bg-black">
+      <AppSectionContainer
+        className="
+          w-full`
+          h-screen
+          max-w-[1400px]
+          flex flex-col items-center 
+          border-[0.5px] border-[rgba(255,255,255,0.1)]
+          shadow-[0_0_40px_10px_rgba(0,0,0,0.3)]
+          bg-black
+          rounded-2xl
+          bg-[linear-gradient(90deg,rgba(0,0,0,1)_0%,rgba(130,12,12,0.18)_14%,rgba(0,0,0,1)_150%)]
+          p-6
+        "
+      >
+        <LizardText className="text-[18px] text-white font-light uppercase mb-8">
+          Craftmanship
+        </LizardText>
+        <div className="flex justify-center gap-2">
+          <LizardCard
+            title="My Services"
+            subtitle="Your short description here."
+            badge="Technology"
+            logoSrc="/assets/lizardinteractive.svg"
+          />
+          <LizardCardGrid cards={centerCard} component={LizardCenterCard} />
+          <LizardCard
+            title="Experiences"
+            subtitle="MY SHORT DESCRIPTION HERE"
+            badge="Technology"
+            logoSrc="/assets/lizardinteractive.svg"
+          />
+        </div>
+      </AppSectionContainer>
+    </AppMainContainer>
+  );
+}
